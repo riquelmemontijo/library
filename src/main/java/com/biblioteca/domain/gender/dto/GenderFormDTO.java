60e83588995cb3fa062e844732959699b05ee2cd
Riquelme Montijo <riquelmesouzam@gmail.com>
2023-03-05 22:41:17 -0300

    Começando a implementar bean validation

--- src/main/java/com/biblioteca/domain/gender/dto/GenderFormDTO.java
+++ src/main/java/com/biblioteca/domain/gender/dto/GenderFormDTO.java
@@ -1,4 +1,9 @@
 package com.biblioteca.domain.gender.dto;
 
-public record GenderFormDTO (String name){
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public record GenderFormDTO (@NotBlank @NotNull @Length(min = 0, max = 20) String name){
 }
