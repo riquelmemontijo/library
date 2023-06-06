fc3c710594ee0f1b9cd84107aa7cdb994a31697d
Riquelme Montijo <riquelmesouzam@gmail.com>
2023-06-05 23:20:42 -0300

    Limpeza de código

--- src/main/java/com/biblioteca/domain/book/dto/BookInBorrowDTO.java
+++ src/main/java/com/biblioteca/domain/book/dto/BookInBorrowDTO.java
@@ -1,6 +1,11 @@
 package com.biblioteca.domain.book.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
-public record BookInBorrowDTO(UUID id, String title) {
+public record BookInBorrowDTO(@NotNull(message = "O id do livro é obrigatório")
+                              UUID id,
+                              @NotBlank(message = "O título do livro é obrigatório")
+                              String title) {
 }
--- src/main/java/com/biblioteca/domain/student/dto/StudentInBorrowDTO.java
+++ src/main/java/com/biblioteca/domain/student/dto/StudentInBorrowDTO.java
@@ -1,6 +1,11 @@
 package com.biblioteca.domain.student.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
-public record StudentInBorrowDTO(UUID id, String name) {
+public record StudentInBorrowDTO(@NotNull(message = "O id do estudante é obrigatorio")
+                                 UUID id,
+                                 @NotBlank(message = "O nome do estudante é obrigatório")
+                                 String name) {
 }
