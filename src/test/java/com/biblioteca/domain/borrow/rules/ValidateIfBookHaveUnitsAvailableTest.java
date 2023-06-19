803da43322610e0718379690651330600a5b9082
Riquelme Montijo <riquelmesouzam@gmail.com>
2023-06-19 12:49:42 -0300

    Teste de unidades disponíveis de livros finalizada

--- src/test/java/com/biblioteca/domain/borrow/rules/ValidateIfBookHaveUnitsAvailableTest.java
+++ src/test/java/com/biblioteca/domain/borrow/rules/ValidateIfBookHaveUnitsAvailableTest.java
@@ -3,13 +3,12 @@ package com.biblioteca.domain.borrow.rules;
 import com.biblioteca.domain.book.Book;
 import com.biblioteca.domain.borrow.Borrow;
 import com.biblioteca.infrastructure.exception.BusinessRulesException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.List;
 
-import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ValidateIfBookHaveUnitsAvailableTest {
 
@@ -19,17 +18,19 @@ public class ValidateIfBookHaveUnitsAvailableTest {
         var book = new Book();
         book.setAvailableUnits(0);
         var borrow = new Borrow();
-        borrow.setBooks(Arrays.asList(book));
+        borrow.setBooks(List.of(book));
 
         assertThrows(BusinessRulesException.class, () -> rule.validate(borrow));
     }
 
     @Test
-    void dontThrowExceptionIfBookDontHaveUnitsAvailable(){
+    void dontThrowExceptionIfBookHaveUnitsAvailable(){
         ValidateIfBookHaveUnitsAvailable rule = new ValidateIfBookHaveUnitsAvailable();
         var book = new Book();
         book.setAvailableUnits(1);
         var borrow = new Borrow();
-        borrow.setBooks(Arrays.asList(book));
+        borrow.setBooks(List.of(book));
+
+        assertDoesNotThrow(() -> rule.validate(borrow));
     }
 }
